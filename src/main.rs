cf71ca9676ad9b3f18610f442bbf70050685f6e9
Youming <dxuanming@sjtu.edu.cn>
2025-06-24 17:42:55 +0800

    image 2

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,35 @@
-use console::style;
-use image::{ImageBuffer, RgbImage};
-use indicatif::ProgressBar;
+use raytracer::camera::Camera;
+use raytracer::ray::Ray;
+use raytracer::sketchpad::Sketchpad;
+use raytracer::vec3::{Point3, Vec3};
 
 fn main() {
-    let path = std::path::Path::new("output/book1/image1.png");
-    let prefix = path.parent().unwrap();
-    std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
-
-    let width = 256;
-    let height = 256;
-    // different from the book, we use image crate to create a .png image rather than outputting .ppm file, which is not widely used.
-    // anyway, you may output any image format you like.
-    let mut img: RgbImage = ImageBuffer::new(width, height);
-
-    let progress = if option_env!("CI").unwrap_or_default() == "true" {
-        ProgressBar::hidden()
-    } else {
-        ProgressBar::new((height * width) as u64)
-    };
-
-    for j in (0..height).rev() {
-        for i in 0..width {
-            let pixel = img.get_pixel_mut(i, j);
-            let r: f64 = (i as f64) / ((width - 1) as f64) * 255.999;
-            let g: f64 = (j as f64) / ((height - 1) as f64) * 255.999;
-            let b: f64 = 0.25 * 255.999;
-            *pixel = image::Rgb([r as u8, g as u8, b as u8]);
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
+    let image_height = if image_height < 1 { 1 } else { image_height };
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
+    let focal_length = 1.0;
+    let camera_center = Point3::new(0.0, 0.0, 0.0);
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let camera = Camera::new(camera_center, viewport_upper_left, viewport_u, viewport_v);
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    let mut sketchpad = Sketchpad::new(image_width as u32, image_height as u32);
+    for j in (0..image_height) {
+        for i in 0..image_width {
+            let pixel_center =
+                pixel00_loc + ((i as f64) * pixel_delta_u) + ((j as f64) * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray::new(camera_center, ray_direction);
+            let pixel_color = r.ray_color();
+            sketchpad.draw(i as u32, j as u32, pixel_color);
         }
-        progress.inc(1);
     }
-    progress.finish();
-
-    println!(
-        "Output image as \"{}\"",
-        style(path.to_str().unwrap()).yellow()
-    );
-    img.save(path).expect("Cannot save the image to the file");
+    sketchpad.save();
 }
---
+++ src/sketchpad.rs
@@ -0,0 +1,42 @@
+use crate::vec3color::Color;
+use console::style;
+use image::{ImageBuffer, RgbImage};
+use indicatif::ProgressBar;
+
+pub struct Sketchpad {
+    image: RgbImage,
+}
+
+impl Sketchpad {
+    pub fn new(width: u32, height: u32) -> Self {
+        Self {
+            image: ImageBuffer::new(width, height),
+        }
+    }
+
+    pub fn draw(&mut self, x: u32, y: u32, color: Color) {
+        let pixel = self.image.get_pixel_mut(x, y);
+        *pixel = image::Rgb([color.r_byte(), color.g_byte(), color.b_byte()]);
+    }
+
+    pub fn width(&self) -> u32 {
+        self.image.width()
+    }
+
+    pub fn height(&self) -> u32 {
+        self.image.height()
+    }
+
+    pub fn save(&self) {
+        let path = std::path::Path::new("output/book1/image2.png");
+        let prefix = path.parent().unwrap();
+        std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
+        println!(
+            "Output image as \"{}\"",
+            style(path.to_str().unwrap()).yellow()
+        );
+        self.image
+            .save(path)
+            .expect("Cannot save the image to the file");
+    }
+}
