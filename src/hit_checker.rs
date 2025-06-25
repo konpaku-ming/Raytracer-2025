d650afc91b7c81c5f91a5a9c3d13f10c139679ca
Youming <dxuanming@sjtu.edu.cn>
2025-06-26 01:30:10 +0800

    image_5

---
+++ src/hit_checker.rs
@@ -0,0 +1,107 @@
+use crate::ray::Ray;
+use crate::vec3::Point3;
+use crate::vec3::Vec3;
+use crate::vec3::dot;
+use std::rc::Rc;
+
+#[derive(Clone, Default)]
+pub struct HitRecord {
+    pub pos: Point3,
+    pub normal: Vec3,
+    pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn new(ray: &Ray, t: f64, outward_normal: Vec3) -> Self {
+        let pos = ray.at(t);
+        let front_face = dot(ray.direction(), &outward_normal) < 0.0;
+        let normal = if front_face {
+            outward_normal
+        } else {
+            -outward_normal
+        };
+
+        Self {
+            t,
+            pos,
+            normal,
+            front_face,
+        }
+    }
+}
+
+pub trait Hittable {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, hit_record: &mut HitRecord) -> bool;
+}
+
+pub struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    pub fn new(center: Point3, radius: f64) -> Self {
+        Self { center, radius }
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, hit_record: &mut HitRecord) -> bool {
+        let oc = self.center - *ray.origin();
+        let a = ray.direction().length_squared();
+        let h = dot(ray.direction(), &oc);
+        let c = oc.length_squared() - self.radius * self.radius;
+        let discriminant = h * h - a * c;
+        if discriminant < 0.0 {
+            return false;
+        }
+        let sqrt_d = discriminant.sqrt();
+        let mut root = (h - sqrt_d) / a;
+        if root <= t_min || t_max <= root {
+            root = (h + sqrt_d) / a;
+            if root <= t_min || t_max <= root {
+                return false;
+            }
+        }
+        hit_record.t = root;
+        hit_record.pos = ray.at(hit_record.t);
+        hit_record.normal = (hit_record.pos - self.center) / self.radius;
+        let outward_normal = (hit_record.pos - self.center) / self.radius;
+        hit_record.front_face = dot(ray.direction(), &outward_normal) < 0.0;
+        hit_record.normal = if hit_record.front_face {
+            outward_normal
+        } else {
+            -outward_normal
+        };
+        true
+    }
+}
+
+#[derive(Default)]
+pub struct HittableList {
+    objects: Vec<Rc<dyn Hittable>>,
+}
+
+impl HittableList {
+    pub fn add(&mut self, object: Rc<dyn Hittable>) {
+        self.objects.push(object);
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord::default();
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for object in &self.objects {
+            if object.hit(r, t_min, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *rec = temp_rec.clone();
+            }
+        }
+        hit_anything
+    }
+}
