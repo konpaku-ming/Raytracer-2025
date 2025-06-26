3e2532ca5d4a0269cdd1a1bf77264590d29261c5
Youming <dxuanming@sjtu.edu.cn>
2025-06-26 12:26:11 +0800

    image_12

--- src/vec3color.rs
+++ src/vec3color.rs
@@ -18,11 +18,22 @@ impl Color {
         (255.999 * b) as u8
     }
 
+    pub fn linear_to_gamma(linear_component: f64) -> f64 {
+        if linear_component > 0.0 {
+            linear_component.sqrt()
+        } else {
+            0.0
+        }
+    }
+
     pub fn write_color(pixel_color: &Color) -> [u8; 3] {
         let intensity = Interval::new(0.000, 0.999);
         let r = pixel_color.x();
+        let r = Self::linear_to_gamma(r);
         let g = pixel_color.y();
+        let g = Self::linear_to_gamma(g);
         let b = pixel_color.z();
+        let b = Self::linear_to_gamma(b);
         let r_byte = (256.0 * intensity.clamp(r)) as u8;
         let g_byte = (256.0 * intensity.clamp(g)) as u8;
         let b_byte = (256.0 * intensity.clamp(b)) as u8;
