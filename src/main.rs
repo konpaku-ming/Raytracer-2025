83edb8de6b2ce2f78d9d420855d50bb0323182ff
Youming <dxuanming@sjtu.edu.cn>
2025-06-26 16:04:25 +0800

    image_16

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use raytracer::hit_checker::HittableList;
 use raytracer::hit_checker::Sphere;
+use raytracer::material::Dielectric;
 use raytracer::material::Lambertian;
 use raytracer::material::Metal;
 use raytracer::raytracer::RayTracer;
@@ -17,7 +18,7 @@ fn main() {
     let mut hittable_list = HittableList::default();
     let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    let material_left = Rc::new(Dielectric::new(1.50));
     let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
     hittable_list.add(Rc::new(Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
--- src/vec3.rs
+++ src/vec3.rs
@@ -80,6 +80,13 @@ impl Vec3 {
     pub fn reflect(v: &Vec3, n: &Vec3) -> Vec3 {
         *v - 2.0 * dot(v, n) * (*n)
     }
+
+    pub fn refract(uv: &Vec3, n: &Vec3, eta_rate: f64) -> Vec3 {
+        let cos_theta = dot(&-*uv, n).min(1.0);
+        let r_out_perp = eta_rate * (*uv + cos_theta * (*n));
+        let r_out_parallel = -*n * (1.0 - r_out_perp.length_squared()).abs().sqrt();
+        r_out_perp + r_out_parallel
+    }
 }
 
 //索引借用分量
