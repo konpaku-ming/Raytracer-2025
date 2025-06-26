df94eace21b75bd8f3cc964903483833f48de091
Youming <dxuanming@sjtu.edu.cn>
2025-06-26 15:10:39 +0800

    image_13

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use raytracer::hit_checker::HittableList;
 use raytracer::hit_checker::Sphere;
+use raytracer::material::Lambertian;
+use raytracer::material::Metal;
 use raytracer::raytracer::RayTracer;
-use raytracer::vec3::Vec3;
+use raytracer::vec3::Point3;
+use raytracer::vec3color::Color;
 use std::rc::Rc;
 
 fn main() {
@@ -12,8 +15,30 @@ fn main() {
     let samples_per_pixel = 100;
     let max_depth = 50;
     let mut hittable_list = HittableList::default();
-    hittable_list.add(Rc::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)));
-    hittable_list.add(Rc::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
+    let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let material_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
+    hittable_list.add(Rc::new(Sphere::new(
+        Point3::new(0.0, -100.5, -1.0),
+        100.0,
+        material_ground,
+    )));
+    hittable_list.add(Rc::new(Sphere::new(
+        Point3::new(0.0, 0.0, -1.2),
+        0.5,
+        material_center,
+    )));
+    hittable_list.add(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.5,
+        material_left,
+    )));
+    hittable_list.add(Rc::new(Sphere::new(
+        Point3::new(1.0, 0.0, -1.0),
+        0.5,
+        material_right,
+    )));
     let mut raytracer = RayTracer::new(
         aspect_ratio,
         image_width,
---
+++ src/material.rs
@@ -0,0 +1,74 @@
+use crate::hit_checker::HitRecord;
+use crate::ray::Ray;
+use crate::vec3::Vec3;
+use crate::vec3color::Color;
+
+pub trait Material {
+    fn scatter(
+        &self,
+        _r_in: &Ray,
+        _rec: &HitRecord,
+        _attenuation: &mut Color,
+        _scattered: &mut Ray,
+    ) -> bool {
+        false
+    }
+}
+
+#[derive(Default)]
+pub struct DummyMaterial;
+
+impl Material for DummyMaterial {}
+
+pub struct Lambertian {
+    pub albedo: Color,
+}
+
+impl Lambertian {
+    pub fn new(albedo: Color) -> Self {
+        Self { albedo }
+    }
+}
+
+impl Material for Lambertian {
+    fn scatter(
+        &self,
+        _r_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Color,
+        scattered: &mut Ray,
+    ) -> bool {
+        let mut scatter_direction = rec.normal + Vec3::random_unit_vector();
+        if scatter_direction.near_zero() {
+            scatter_direction = rec.normal;
+        }
+        *scattered = Ray::new(rec.pos, scatter_direction);
+        *attenuation = self.albedo;
+        true
+    }
+}
+
+pub struct Metal {
+    pub albedo: Color,
+}
+
+impl Metal {
+    pub fn new(albedo: Color) -> Self {
+        Self { albedo }
+    }
+}
+
+impl Material for Metal {
+    fn scatter(
+        &self,
+        r_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Color,
+        scattered: &mut Ray,
+    ) -> bool {
+        let reflected = Vec3::reflect(r_in.direction(), &rec.normal);
+        *scattered = Ray::new(rec.pos, reflected);
+        *attenuation = self.albedo;
+        true
+    }
+}
