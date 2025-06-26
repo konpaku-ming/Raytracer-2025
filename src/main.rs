01ac245ca42d9612aab822b9802f94ea7a70e0d1
Youming <dxuanming@sjtu.edu.cn>
2025-06-26 18:39:04 +0800

    image_19

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use raytracer::hit_checker::HittableList;
 use raytracer::hit_checker::Sphere;
-use raytracer::material::Dielectric;
+//use raytracer::material::Dielectric;
 use raytracer::material::Lambertian;
-use raytracer::material::Metal;
+//use raytracer::material::Metal;
 use raytracer::raytracer::RayTracer;
 use raytracer::vec3::Point3;
 use raytracer::vec3color::Color;
@@ -11,49 +11,35 @@ use std::rc::Rc;
 fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
-    let viewport_height = 2.0;
+    //let viewport_height = 2.0;
     let focal_length = 1.0;
     let samples_per_pixel = 100;
     let max_depth = 50;
+    let v_fov = 90.0;
     let mut hittable_list = HittableList::default();
-    let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Rc::new(Dielectric::new(1.50));
-    let material_bubble = Rc::new(Dielectric::new(1.00 / 1.50));
-    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
+    let r = (std::f64::consts::PI / 4.0).cos();
+
+    let material_left = Rc::new(Lambertian::new(Color::new(0.0, 0.0, 1.0)));
+    let material_right = Rc::new(Lambertian::new(Color::new(1.0, 0.0, 0.0)));
     hittable_list.add(Rc::new(Sphere::new(
-        Point3::new(0.0, -100.5, -1.0),
-        100.0,
-        material_ground,
-    )));
-    hittable_list.add(Rc::new(Sphere::new(
-        Point3::new(0.0, 0.0, -1.2),
-        0.5,
-        material_center,
-    )));
-    hittable_list.add(Rc::new(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.5,
+        Point3::new(-r, 0.0, -1.0),
+        r,
         material_left,
     )));
     hittable_list.add(Rc::new(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.4,
-        material_bubble,
-    )));
-    hittable_list.add(Rc::new(Sphere::new(
-        Point3::new(1.0, 0.0, -1.0),
-        0.5,
+        Point3::new(r, 0.0, -1.0),
+        r,
         material_right,
     )));
     let mut raytracer = RayTracer::new(
         aspect_ratio,
         image_width,
-        viewport_height,
+        //viewport_height,
         focal_length,
         hittable_list,
         samples_per_pixel,
         max_depth,
+        v_fov,
     );
     raytracer.render();
 }
