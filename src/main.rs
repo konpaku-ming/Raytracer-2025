387b149827bcdacf72fafd1d269949c2290fb130
Youming <dxuanming@sjtu.edu.cn>
2025-06-26 16:55:37 +0800

    image_14 remake

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use raytracer::hit_checker::HittableList;
 use raytracer::hit_checker::Sphere;
-use raytracer::material::Dielectric;
 use raytracer::material::Lambertian;
 use raytracer::material::Metal;
 use raytracer::raytracer::RayTracer;
@@ -18,8 +17,7 @@ fn main() {
     let mut hittable_list = HittableList::default();
     let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Rc::new(Dielectric::new(1.50));
-    let material_bubble = Rc::new(Dielectric::new(1.00 / 1.50));
+    let material_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
     let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
     hittable_list.add(Rc::new(Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
@@ -35,11 +33,6 @@ fn main() {
         Point3::new(-1.0, 0.0, -1.0),
         0.5,
         material_left,
-    )));
-    hittable_list.add(Rc::new(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.4,
-        material_bubble,
     )));
     hittable_list.add(Rc::new(Sphere::new(
         Point3::new(1.0, 0.0, -1.0),
--- src/material.rs
+++ src/material.rs
@@ -1,5 +1,4 @@
 use crate::hit_checker::HitRecord;
-use crate::random::random_double;
 use crate::ray::Ray;
 use crate::vec3::Vec3;
 use crate::vec3::dot;
@@ -78,49 +77,5 @@ impl Material for Metal {
         *scattered = Ray::new(rec.pos, reflected);
         *attenuation = self.albedo;
         dot(scattered.direction(), &rec.normal) > 0.0
-    }
-}
-
-pub struct Dielectric {
-    pub refraction_index: f64,
-}
-
-impl Dielectric {
-    pub fn new(refraction_index: f64) -> Self {
-        Self { refraction_index }
-    }
-}
-
-fn reflectance(cosine: f64, refraction_index: f64) -> f64 {
-    let r0 = (1.0 - refraction_index) / (1.0 + refraction_index);
-    let r0 = r0 * r0;
-    r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
-}
-
-impl Material for Dielectric {
-    fn scatter(
-        &self,
-        r_in: &Ray,
-        rec: &HitRecord,
-        attenuation: &mut Color,
-        scattered: &mut Ray,
-    ) -> bool {
-        *attenuation = Color::new(1.0, 1.0, 1.0);
-        let ri = if rec.front_face {
-            1.0 / self.refraction_index
-        } else {
-            self.refraction_index
-        };
-        let unit_direction = unit_vector(r_in.direction());
-        let cos_theta = dot(&-unit_direction, &rec.normal).min(1.0);
-        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
-        let cannot_refract = ri * sin_theta > 1.0; //全反射
-        let direction = if cannot_refract || reflectance(cos_theta, ri) > random_double() {
-            Vec3::reflect(&unit_direction, &rec.normal)
-        } else {
-            Vec3::refract(&unit_direction, &rec.normal, ri)
-        };
-        *scattered = Ray::new(rec.pos, direction);
-        true
     }
 }
