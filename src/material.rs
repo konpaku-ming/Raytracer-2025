a1e0c29957fcf10e2116c253e03d18e477ce5330
Youming <dxuanming@sjtu.edu.cn>
2025-06-26 17:04:02 +0800

    image_16 remake

--- src/material.rs
+++ src/material.rs
@@ -79,3 +79,35 @@ impl Material for Metal {
         dot(scattered.direction(), &rec.normal) > 0.0
     }
 }
+
+pub struct Dielectric {
+    pub refraction_index: f64,
+}
+
+impl Dielectric {
+    pub fn new(refraction_index: f64) -> Self {
+        Self { refraction_index }
+    }
+}
+
+impl Material for Dielectric {
+    fn scatter(
+        &self,
+        r_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Color,
+        scattered: &mut Ray,
+    ) -> bool {
+        *attenuation = Color::new(1.0, 1.0, 1.0);
+        let ri = if rec.front_face {
+            1.0 / self.refraction_index
+        } else {
+            self.refraction_index
+        };
+
+        let unit_direction = unit_vector(r_in.direction());
+        let refracted = Vec3::refract(&unit_direction, &rec.normal, ri);
+        *scattered = Ray::new(rec.pos, refracted);
+        true
+    }
+}
