8ab5af044fc9fb4adefe7f6d2e49d8ca5ff3871f
Youming <dxuanming@sjtu.edu.cn>
2025-06-29 11:01:14 +0800

    image_2.10

--- src/perlin.rs
+++ src/perlin.rs
@@ -26,12 +26,27 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = ((4.0 * p.x()) as i32 & 255) as usize;
-        let j = ((4.0 * p.y()) as i32 & 255) as usize;
-        let k = ((4.0 * p.z()) as i32 & 255) as usize;
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
-        let idx = (self.perm_x[i] ^ self.perm_y[j]) ^ self.perm_z[k];
-        self.rand_float[idx as usize]
+        let i = p.x().floor() as i32;
+        let j = p.y().floor() as i32;
+        let k = p.z().floor() as i32;
+
+        let mut c = [[[0.0f64; 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    let idx = self.perm_x[((i + di) & 255) as usize]
+                        ^ self.perm_y[((j + dj) & 255) as usize]
+                        ^ self.perm_z[((k + dk) & 255) as usize];
+                    c[di as usize][dj as usize][dk as usize] = self.rand_float[idx as usize];
+                }
+            }
+        }
+        Perlin::trilinear(c, u, v, w)
     }
 
     fn generate_perm() -> [i32; POINT_COUNT] {
@@ -48,6 +63,21 @@ impl Perlin {
             let target = random_int_range(0, i as i32);
             p.swap(i, target as usize);
         }
+    }
+
+    fn trilinear(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    let weight_u = if i == 1 { u } else { 1.0 - u };
+                    let weight_v = if j == 1 { v } else { 1.0 - v };
+                    let weight_w = if k == 1 { w } else { 1.0 - w };
+                    accum += weight_u * weight_v * weight_w * c[i][j][k];
+                }
+            }
+        }
+        accum
     }
 }
 impl Default for Perlin {
