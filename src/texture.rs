8c9e5bc8850c15198d08e15f71bcaf791dbfa496
Youming <dxuanming@sjtu.edu.cn>
2025-06-29 11:37:39 +0800

    image 2.12

--- src/texture.rs
+++ src/texture.rs
@@ -103,25 +103,27 @@ impl Texture for ImageTexture {
 
 pub struct NoiseTexture {
     noise: Perlin,
+    scale: f64,
 }
 
 impl NoiseTexture {
-    pub fn new() -> Self {
+    pub fn new(scale: f64) -> Self {
         Self {
             noise: Perlin::new(),
+            scale,
         }
     }
 }
 
 impl Texture for NoiseTexture {
     fn value(&self, _u: f64, _v: f64, p: &Point3) -> Color {
-        let noise_float = self.noise.noise(p);
+        let noise_float = self.noise.noise(&(self.scale * (*p)));
         Color::new(noise_float, noise_float, noise_float)
     }
 }
 
 impl Default for NoiseTexture {
     fn default() -> Self {
-        Self::new()
+        Self::new(1.0)
     }
 }
