86afb8bf46ab15ab602b60f9b4369c281743787a
Youming <dxuanming@sjtu.edu.cn>
2025-06-30 12:03:12 +0800

    image 2.20

--- src/hit_checker.rs
+++ src/hit_checker.rs
@@ -260,3 +260,52 @@ impl Hittable for Quad {
         self.bbox
     }
 }
+
+pub fn make_box(a: Point3, b: Point3, mat: Rc<dyn Material>) -> Rc<HittableList> {
+    let mut sides = HittableList::default();
+
+    let min = Point3::new(a.x().min(b.x()), a.y().min(b.y()), a.z().min(b.z()));
+    let max = Point3::new(a.x().max(b.x()), a.y().max(b.y()), a.z().max(b.z()));
+
+    let dx = Vec3::new(max.x() - min.x(), 0.0, 0.0);
+    let dy = Vec3::new(0.0, max.y() - min.y(), 0.0);
+    let dz = Vec3::new(0.0, 0.0, max.z() - min.z());
+
+    sides.add(Rc::new(Quad::new(
+        Point3::new(min.x(), min.y(), max.z()),
+        dx,
+        dy,
+        mat.clone(), //front
+    )));
+    sides.add(Rc::new(Quad::new(
+        Point3::new(max.x(), min.y(), max.z()),
+        -dz,
+        dy,
+        mat.clone(), //right
+    )));
+    sides.add(Rc::new(Quad::new(
+        Point3::new(max.x(), min.y(), min.z()),
+        -dx,
+        dy,
+        mat.clone(), //back
+    )));
+    sides.add(Rc::new(Quad::new(
+        Point3::new(min.x(), min.y(), min.z()),
+        dz,
+        dy,
+        mat.clone(), //left
+    )));
+    sides.add(Rc::new(Quad::new(
+        Point3::new(min.x(), max.y(), max.z()),
+        dx,
+        -dz,
+        mat.clone(), //top
+    )));
+    sides.add(Rc::new(Quad::new(
+        Point3::new(min.x(), min.y(), min.z()),
+        dx,
+        dz,
+        mat.clone(), //bottom
+    )));
+    Rc::new(sides)
+}
