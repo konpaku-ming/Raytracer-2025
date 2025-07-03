e6cb8d20ad24d96ad5f7246540982c9aaec55353
Youming <dxuanming@sjtu.edu.cn>
2025-07-03 14:38:23 +0800

    image 3.15

--- src/vec3color.rs
+++ src/vec3color.rs
@@ -28,12 +28,21 @@ impl Color {
 
     pub fn write_color(pixel_color: &Color) -> [u8; 3] {
         let intensity = Interval::new(0.000, 0.999);
-        let r = pixel_color.x();
-        let r = Self::linear_to_gamma(r);
-        let g = pixel_color.y();
-        let g = Self::linear_to_gamma(g);
-        let b = pixel_color.z();
-        let b = Self::linear_to_gamma(b);
+        let mut r = pixel_color.x();
+        if r.is_nan() {
+            r = 0.0
+        };
+        r = Self::linear_to_gamma(r);
+        let mut g = pixel_color.y();
+        if g.is_nan() {
+            g = 0.0
+        };
+        g = Self::linear_to_gamma(g);
+        let mut b = pixel_color.z();
+        if b.is_nan() {
+            b = 0.0
+        };
+        b = Self::linear_to_gamma(b);
         let r_byte = (256.0 * intensity.clamp(r)) as u8;
         let g_byte = (256.0 * intensity.clamp(g)) as u8;
         let b_byte = (256.0 * intensity.clamp(b)) as u8;
